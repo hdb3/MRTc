d3faef3a27f1e2cc633fc56de4a3343d23310ba5
nic hart <nic@hdb3.net>
2019-09-11 11:54:27 +0100

    fix .h file list

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 clang-format -i libmrtcommon.c libmrtextra.c libmrttabledump.c libmrtupdates.c mrttest2.c mrtupdatetest.c mrttabledumptest.c
-clang-format -i libmrtcommon.h libmrtdef.h libmrtextra.h libmrt.h libmrttabledump.h libmrtupdates.h
+clang-format -i libmrtcommon.h libmrtextra.h libmrt.h libmrttabledump.h libmrtupdates.h
 gcc -g -O2 -c libmrtcommon.c libmrtextra.c libmrttabledump.c libmrtupdates.c
 ar -cr libmrt.a libmrtcommon.o libmrtextra.o libmrttabledump.o libmrtupdates.o
 gcc -g -O2 -o mrttest2 mrttest2.c libmrt.a
