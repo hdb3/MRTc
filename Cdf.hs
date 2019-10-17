15cfb172267305e5b405d5084920500455bbd3ec
nic hart <nic@hdb3.net>
2019-10-17 18:58:37 +0100

    working version for a single configurable column, no remaning value calculation yet

--- Cdf.hs
+++ Cdf.hs
@@ -1,24 +1,18 @@
 module Main where
 import qualified Data.ByteString.Lazy.Char8 as C
 import System.Environment(getArgs)
+import qualified Data.IntMap as Map
+import Data.List(foldl')
 
 main = do
   args <- getArgs
   let n = read (args !! 1) :: Int
   f <- C.readFile (head args)
-  -- trivial n f
-  fullFat n f
-
-trivial n f = do
-  args <- getArgs
-  let l = C.lines f !! m
-      x = index n l
-      m = read (args !! 2) :: Int
-  C.putStrLn l
-  print x
-
-fullFat n f = mapM_ (extract n) (C.lines f)
+  let nx = map (index n) (C.lines f)
+  let f m v = Map.insertWith (+) v 1 m
+      m = foldl' f Map.empty nx
+      lm = Map.toAscList m
+  mapM_ print lm
 
 index n l = readInt $ (!!n) $ C.words l
 readInt s = n where Just (n,_) = C.readInt s
-extract n l = print ( index n l)
