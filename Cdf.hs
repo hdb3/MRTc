54a5ee2daa9c55b0f6f8a277bf7dc328d07b9d68
Nicholas Hart <nic@hdb3.net>
2019-10-18 07:56:30 +0100

    finished accumulate

--- Cdf.hs
+++ Cdf.hs
@@ -12,7 +12,13 @@ main = do
   let f m v = Map.insertWith (+) v 1 m
       m = foldl' f Map.empty nx
       lm = Map.toAscList m
+      alm = accumulate lm
   mapM_ print lm
+  mapM_ print alm
 
 index n l = readInt $ (!!n) $ C.words l
 readInt s = n where Just (n,_) = C.readInt s
+
+accumulate = go 0 where
+    go z ( (k,c):kcx) | null kcx = [(k,z+c)]
+                      | otherwise = (k,z+c) : ( go (z+c) kcx )
