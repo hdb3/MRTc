e1c97461d2d57740e0f119cd85cfcadc6f24b7b8
Nicholas Hart <nic@hdb3.net>
2019-10-21 10:32:41 +0100

    completed for percentils and raw graph data - could still improve on graph scripts

--- Cdf.hs
+++ Cdf.hs
@@ -13,38 +13,41 @@ import System.IO(openFile,IOMode(WriteMode),hPutStrLn,hClose)
 -}
 main = do
   args <- getArgs
-  let n = read (args !! 1) :: Int
-  f <- C.readFile (head args)
-  let (header:content) = C.lines f
-      columnNames = map C.unpack $ C.words header
-      readInt s = n where Just (n,_) = C.readInt s
-      fieldList = map (map readInt . C.words) content
-      emptyMaps = replicate (length columnNames) (Map.empty :: Map.IntMap Int)
-      updater :: Map.IntMap Int -> Int -> Map.IntMap Int
-      updater m n = Map.insertWith (+) n 1 m
-      updater2 :: [Map.IntMap Int] -> [Int] -> [Map.IntMap Int]
-      updater2 = zipWith updater
-      parsedContent = foldl updater2 emptyMaps fieldList
-      aggregates = zip columnNames (map Map.toAscList parsedContent)
-  putStrLn $ "read the following columns: " ++ unwords columnNames
-  mapM_ processCumulative aggregates
-  mapM_ processPad aggregates
+  aggregate <- getAggregate (head args)
+  mapM_ processCumulative aggregate
+  mapM_ processPad aggregate
+  mapM_ processPercentiles aggregate
+
+getAggregate :: String -> IO [(String, [(Int, Int)])]
+getAggregate fn = do
+    f <- C.readFile fn
+    let (header:content) = C.lines f
+        columnNames = map C.unpack $ C.words header
+        readInt s = n where Just (n,_) = C.readInt s
+        fieldList = map (map readInt . C.words) content
+        emptyMaps = replicate (length columnNames) (Map.empty :: Map.IntMap Int)
+        updater :: Map.IntMap Int -> Int -> Map.IntMap Int
+        updater m n = Map.insertWith (+) n 1 m
+        updater2 :: [Map.IntMap Int] -> [Int] -> [Map.IntMap Int]
+        updater2 = zipWith updater
+        parsedContent = foldl updater2 emptyMaps fieldList
+        aggregate = zip columnNames (map Map.toAscList parsedContent)
+    putStrLn $ "read the following columns: " ++ unwords columnNames
+    return aggregate
+
+accumulate :: [(Int,Int)] -> [(Int,Int,Int)]
+accumulate = go 0 where
+go z ( (k,c):kcx) | null kcx = [(k,c,z+c)]
+                  | otherwise = (k,c,z+c) : go (z+c) kcx
 
 processCumulative (name,rows) = do 
 
     let 
-        accumulate = go 0 where
-            go z ( (k,c):kcx) | null kcx = [(k,c,z+c)]
-                              | otherwise = (k,c,z+c) : go (z+c) kcx
-
-        cumulativeRows :: [(Int,Int,Int)]
         cumulativeRows = accumulate rows
-
         (_,_,sampleCount) = last cumulativeRows
         sampleCountFloat = fromIntegral sampleCount
-
-        normedSamples :: [(Int,Int,Int,Double)]
         normedSamples = map (\(k,c,z) -> (k,c,z,(sampleCountFloat - fromIntegral z)/sampleCountFloat)) cumulativeRows
+    
     putStr $ "processColumn: " ++ name
     putStr $ "read " ++ show (length rows) ++ " rows, "
     putStrLn $ show sampleCount ++ " samples"
@@ -57,16 +60,14 @@ processCumulative (name,rows) = do
 
 
 {-
-  design for inserting zeroes for empty slots.
-  This probably only makes sense for the simple accumulated values,
-  though by adding the slots at the first stage the other derivateives come for free...
-
-  Whilst there may be an obvious fold based solution, it is not very obvious,
-  and this even more basic method is simple.
+  Pad: inserting zeroes for empty slots.
+  This probably only makes sense for simple accumulated values,
+  though by adding the slots at the first stage the other derivatives come for free...
   Assume the input is a sorted list on keys: [(key,count)]
-  scan the list with an incrementing index.  Consume the list head when the index is equal the key,
-other wise produce a zero count tuple.
-                      -}
+  - scan the list with an incrementing index.  Consume the list head when the index is equal the key,
+  - otherwise produce a zero count tuple.
+-}
+
 pad :: [(Int,Int)] -> [(Int,Int)]
 pad = go 0 where
     go n ((k,c):kvs) | null kvs = [(k,c)]
@@ -82,3 +83,24 @@ processPad (name,rows) = do
    mapM_ (printRow outfile) (pad rows)
    putStrLn $ "written to " ++ fn
    hClose outfile
+
+percentile :: (Int,Int) -> [(Int,Int)] -> Int
+percentile (m,n) kvs = scan threshold kvs where
+  max = snd $ last kvs
+  threshold = max - (max * m `div` n)
+  scan t ((k,v):kvs) | null kvs = k
+                     | v < t = scan t kvs
+                     | otherwise = k
+
+
+processPercentiles (name,rows) = do
+    putStr $ "percentiles: " ++ name
+    let breaks = [ ("median",(1,2))
+                 , ("90%",(1,10))
+                 , ("99%",(1,100))
+                 , ("99.9%",(1,1000))
+                 , ("max",(0,1))
+                 ]
+        cumulativeRows = map (\(a,_,b) -> (a,b)) $ accumulate rows         
+        percentiles = map (\(label,ratio) -> (label,percentile ratio cumulativeRows)) breaks
+    putStrLn $ unwords $ map show percentiles
