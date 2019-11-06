0cfd5ef380ea20023532b9a49d582e679e67cd27
nic hart <nic@hdb3.net>
2019-11-06 18:17:55 +0000

    stop using archive object file in build.sh because I want the build line compile constats to be propagated.  Plus it is simpler, if slower.

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
-clang-format -i libmrtcommon.c libmrtextra.c libmrttabledump.c libmrtupdates.c mrttest2.c mrtupdatetest.c mrttabledumptest.c libmrttabledumpextra.c
+LIBS="libmrtcommon.c libmrtextra.c libmrttabledump.c libmrtupdates.c libmrttabledumpextra.c"
+HEADERS="libmrtcommon.h libmrtextra.h libmrt.h libmrttabledump.h libmrtupdates.h"
+APPS="mrttest2.c mrtupdatetest.c mrttabledumptest.c main.c bgpparse.c"
+clang-format -i $LIBS $HEADERS
 clang-format -i libmrtcommon.h libmrtextra.h libmrt.h libmrttabledump.h libmrtupdates.h
-gcc -g -O2 -c libmrtcommon.c libmrtextra.c libmrttabledump.c libmrtupdates.c libmrttabledumpextra.c timespec.c
-rm -f libmrt.a
-ar -cr libmrt.a libmrtcommon.o libmrtextra.o libmrttabledump.o libmrtupdates.o libmrttabledumpextra.o timespec.o
-rm libmrtcommon.o libmrtextra.o libmrttabledump.o libmrtupdates.o libmrttabledumpextra.o
-gcc -g -O2 -o mrttest2 mrttest2.c libmrt.a
-gcc -g -O2 -o mrtupdatetest  mrtupdatetest.c libmrt.a
-gcc -DBUILD_UPDATE_LIST=1 -g -O2 -o mrtupdatetest.BUILDLIST  mrtupdatetest.c libmrt.a
-gcc -DNOUPDATE -g -O2 -o mrtupdatetest.NOUPDATE  mrtupdatetest.c libmrt.a
-gcc -DNOPPA -g -O2 -o mrtupdatetest.NOPPA  mrtupdatetest.c libmrt.a
-gcc -DNOSTATS -g -O2 -o mrtupdatetest.NOSTATS  mrtupdatetest.c libmrt.a
-gcc -DNOSTATS -DNOPPA -g -O2 -o mrtupdatetest.NOSTATSORPPA  mrtupdatetest.c libmrt.a
-gcc -g -O2 -o mrttabledumptest mrttabledumptest.c libmrt.a
-gcc -g -O2 -o main main.c libmrt.a
-gcc -g -O2  -o bgpparse bgpparse.c timespec.c
+LIBS+=" timespec.c"
+# gcc -g -O2 -c libmrtcommon.c libmrtextra.c libmrttabledump.c libmrtupdates.c libmrttabledumpextra.c timespec.c
+# rm -f libmrt.a
+# ar -cr libmrt.a libmrtcommon.o libmrtextra.o libmrttabledump.o libmrtupdates.o libmrttabledumpextra.o timespec.o
+# rm libmrtcommon.o libmrtextra.o libmrttabledump.o libmrtupdates.o libmrttabledumpextra.o
+gcc -g -O2 -o mrttest2 mrttest2.c $LIBS
+gcc -g -O2 -o mrtupdatetest mrtupdatetest.c $LIBS
+gcc -DBUILD_UPDATE_LIST=1 -g -O2 -o mrtupdatetest.BUILDLIST mrtupdatetest.c $LIBS
+gcc -DNOUPDATE -g -O2 -o mrtupdatetest.NOUPDATE mrtupdatetest.c $LIBS
+gcc -DNOPPA -g -O2 -o mrtupdatetest.NOPPA mrtupdatetest.c $LIBS
+gcc -DNOSTATS -g -O2 -o mrtupdatetest.NOSTATS mrtupdatetest.c $LIBS
+gcc -DNOSTATS -DNOPPA -g -O2 -o mrtupdatetest.NOSTATSORPPA mrtupdatetest.c $LIBS
+gcc -g -O2 -o mrttabledumptest mrttabledumptest.c $LIBS
+gcc -g -O2 -o main main.c $LIBS
+gcc -g -O2 -o bgpparse bgpparse.c timespec.c
